2d78b0398ae2b10502ef30826bd3bdaff3462626
Mike Axtman <mike.axtman@formidable.com>
2016-05-19 13:28:35 -0700

    these changes are to show working examples of ecology with dropdowns. Will be reverted, not intended for PR

--- demo/docs.jsx
+++ demo/docs.jsx
@@ -7,6 +7,14 @@ import SampleClass from "./sample";
 
 import "./styles.styl";
 
+const option1 = "hello";
+const option2 = "greetings";
+const option3 = "goodbye";
+const option4 = "dat";
+const option5 = "be";
+const option6 = "cool";
+
+
 class Docs extends React.Component {
   render() {
     return (
@@ -14,7 +22,17 @@ class Docs extends React.Component {
         <Ecology
           overview={require("!!raw!./ecology.md")}
           source={docgen.parse(require("!!raw!./sample"))}
-          scope={{React, ReactDOM, SampleClass}}
+          scope={{
+            React,
+            ReactDOM,
+            SampleClass,
+            option1,
+            option2,
+            option3,
+            option4,
+            option5,
+            option6
+          }}
           playgroundtheme="blackboard"/>
       </div>
     );
--- demo/ecology.md
+++ demo/ecology.md
@@ -1,11 +1,23 @@
 SampleClass
 ===========================
 
-```playground_norender
+```playground_norender_dropdown=[option1, option2, option3]
 var App = React.createClass({
   render() {
       return (
-      <SampleClass/>
+      <SampleClass name={option1}/>
+      );
+    }
+})
+
+ReactDOM.render(<App/>, mountNode);
+```
+
+```playground_norender_dropdown=[option4, option5, option6]
+var App = React.createClass({
+  render() {
+      return (
+      <SampleClass name={option4}/>
       );
     }
 })
